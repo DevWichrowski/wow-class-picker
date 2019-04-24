f795c96a3e1986ed754314bf3867c80925462f80
devWichrowski <wichrowski.patryk92@gmail.com>
2019-04-24 21:40:11 +0200

    Minior changes

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -9,28 +9,41 @@ export class MainContainer extends Component {
 		super(props);
 
 		this.state = {
-			intervals: 50,
-			currentIcon: this.props.classes[0].icon,
-			currentClassName: this.props.classes[0].icon
+			intervals: 30,
+			currentClassIcon: this.props.classes[0].icon,
+			currentClassName: null
 		};
 	}
 
+	clearState = () => {
+		this.setState({ intervals: 30 });
+		this.setState({ currentClassName: null });
+	};
+
 	rollIcons = () => {
+		this.clearState();
+
 		const roller = setInterval(() => {
-			this.setState({ currentIcon: this.props.classes[Math.floor(Math.random() * 11 + 1)].icon });
+			this.setState({ currentClassIcon: this.props.classes[Math.floor(Math.random() * 11 + 1)].icon });
 			this.setState({ intervals: this.state.intervals - 1 });
 
 			if (this.state.intervals <= 0) {
+				const randomClass = Math.floor(Math.random() * 11 + 1);
 				clearInterval(roller);
+				this.setState({
+					currentClassName: this.props.classes[randomClass].name,
+					currentClassIcon: this.props.classes[randomClass].icon
+				});
 			}
-		}, 80);
+		}, 100);
 	};
 
 	render() {
 		return (
 			<div className="main-container">
-				<p className="text">Wow random class picker</p>
-				<ClassIcon image={this.state.currentIcon} />
+				<h1 className="text">WoW random class picker</h1>
+				<ClassIcon image={this.state.currentClassIcon} />
+				<h2 className="text">{this.state.currentClassName}</h2>
 				<Button roll={() => this.rollIcons()} />
 			</div>
 		);
