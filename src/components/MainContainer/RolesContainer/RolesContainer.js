25e29519db16fe5e1205af8cf4db7d1dd327bd21
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-04-28 21:05:01 +0100

    Working roles

--- src/components/MainContainer/RolesContainer/RolesContainer.js
+++ src/components/MainContainer/RolesContainer/RolesContainer.js
@@ -1,37 +1,60 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './RolesContainer.scss';
 import Tank_icon from '../../../assets/roles_icons/Tank_icon.jpg';
 import Ranged_icon from '../../../assets/roles_icons/Ranged_icon.jpg';
 import Melee_icon from '../../../assets/roles_icons/Melee_icon.jpg';
 import Healer_icon from '../../../assets/roles_icons/Healer_icon.jpg';
+import connect from "react-redux/es/connect/connect";
+import {getIsHealer, getIsMeleeDps, getIsRangedDps, getIsTankSelector} from "../../../store/selectors/races.selector";
+import {
+    handleHealer,
+    handleMeleeDps,
+    handleRangedDps,
+    handleTank
+} from "../../../store/actions/racesActions";
 
-export class RolesContainer extends Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<div className="roles-container">
-            <h4 className="text">Choose yor role</h4>
-				<div className="roles">
-					<div className="roles">
-						<div className="role-row">
-							<img className="role-image" src={Tank_icon} />
-						</div>
-                        <div className="role-row">
-							<img className="role-image" src={Ranged_icon} />
-						</div>
-                        <div className="role-row">
-							<img className="role-image" src={Melee_icon} />
-						</div>
-                        <div className="role-row">
-							<img className="role-image" src={Healer_icon} />
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+class RolesContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="roles-container">
+                <h4 className="text">Choose yor role</h4>
+                <div className="roles">
+                    <div className="roles">
+                        <div className="role-row" onClick={this.props.handleTank}>
+                            <img className={`role-image ${this.props.isTank ? 'selected' : null}`} src={Tank_icon}/>
+                        </div>
+                        <div className="role-row" onClick={this.props.handleRangedDps}>
+                            <img className={`role-image ${this.props.isRangedDps ? 'selected' : null}`} src={Ranged_icon}/>
+                        </div>
+                        <div className="role-row" onClick={this.props.handleMeleeDps}>
+                            <img className={`role-image ${this.props.isMeleeDps ? 'selected' : null}`} src={Melee_icon}/>
+                        </div>
+                        <div className="role-row" onClick={this.props.handleHealer}>
+                            <img className={`role-image ${this.props.isHealer ? 'selected' : null}`} src={Healer_icon}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default RolesContainer;
+const mapStateToProps = (state) => ({
+    isTank: getIsTankSelector(state),
+    isRangedDps: getIsRangedDps(state),
+    isMeleeDps: getIsMeleeDps(state),
+    isHealer: getIsHealer(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    handleTank: () => dispatch(handleTank()),
+    handleRangedDps: () => dispatch(handleRangedDps()),
+    handleMeleeDps: () => dispatch(handleMeleeDps()),
+    handleHealer: () => dispatch(handleHealer())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(RolesContainer)
