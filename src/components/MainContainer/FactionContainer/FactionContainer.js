6d12b60b2dd75b88e1cd1947a7cf575b75823943
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-04-28 20:30:02 +0100

    Added handleActions to reducers switch and add reselect lib

--- src/components/MainContainer/FactionContainer/FactionContainer.js
+++ src/components/MainContainer/FactionContainer/FactionContainer.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Horde_icon from '../../../assets/faction_icons/Horde_icon.png';
 import Alliance_icon from '../../../assets/faction_icons/Alliance_icon.png';
 import './FactionContainer.scss';
+import connect from "react-redux/es/connect/connect";
+import {handleAlliance, handleHorde} from "../../../store/actions/racesActions";
 
 export class FactionContainer extends Component {
 	constructor(props) {
@@ -13,11 +15,11 @@ export class FactionContainer extends Component {
 			<div className="faction-container">
 				<h4 className="text">Choose your faction</h4>
 				<div className="factions">
-					<div className="alliance-row">
-						<img className="faction-image" src={Alliance_icon} />
+					<div className="alliance-row" onClick={this.props.handleAlliance}>
+						<img className="faction-image " src={Alliance_icon} />
 					</div>
 					<div className="horde-row">
-						<img className="faction-image" src={Horde_icon} />
+						<img className="faction-image" src={Horde_icon} onClick={this.props.handleHorde}/>
 					</div>
 				</div>
 			</div>
@@ -25,4 +27,14 @@ export class FactionContainer extends Component {
 	}
 }
 
-export default FactionContainer;
+const mapStateToProps = (state) => ({
+    classes: state.races.classes,
+    advancedMode: state.races.advancedMode
+});
+
+const mapDispatchToProps = (dispatch) => ({
+	handleAlliance: () => dispatch(handleAlliance()),
+    handleHorde: () => dispatch(handleHorde()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(FactionContainer);
--- src/store/reducers/racesReducer.js
+++ src/store/reducers/racesReducer.js
@@ -3,33 +3,54 @@ import produce from 'immer';
 import {racesData} from "../racesData";
 
 const initialState = {
-	advancedMode: false,
-	factionAlliance: false,
-	factionHorde: false,
-	isTank:  false,
-	isRangedDps: false,
-	isMeleeDps: false,
-	isHealer: false,
+    advancedMode: false,
+    factionAlliance: false,
+    factionHorde: false,
+    isTank: false,
+    isRangedDps: false,
+    isMeleeDps: false,
+    isHealer: false,
 
-	filteredRaces: [],
+    filteredRaces: [],
 
-	races: racesData,
+    races: racesData,
 };
 
 export const racesReducer = (state = initialState, action) =>
-	produce(state, (draft) =>
-		produce(state, (draft) => {
-			switch (action.type) {
-				case RacesActions.HANDLE_SWITCH: {
-					draft.advancedMode = !state.advancedMode;
-					break;
-				}
-				case RacesActions.HANDLE_ALLIANCE: {
-                    draft.factionAlliance = !state.factionAlliance;
+    produce(state, (draft) =>
+        produce(state, (draft) => {
+            switch (action.type) {
+                case RacesActions.HANDLE_SWITCH: {
+                    draft.advancedMode = !state.advancedMode;
+                    break;
                 }
-                
-				default:
-					return state;
-			}
-		})
-	);
+                case RacesActions.HANDLE_ALLIANCE: {
+                    draft.factionAlliance = !state.factionAlliance;
+                    break;
+                }
+                case RacesActions.HANDLE_HORDE: {
+                    draft.factionHorde = !state.factionHorde;
+                    break;
+                }
+                case RacesActions.HANDLE_TANK: {
+                    draft.isTank = !state.isTank;
+                    break;
+                }
+                case RacesActions.HANDLE_RANGED_DPS: {
+                    draft.isRangedDps = !state.isRangedDps;
+                    break;
+                }
+                case RacesActions.HANDLE_MELEE_DPS: {
+                    draft.isMeleeDps = !state.isMeleeDps;
+                    break;
+                }
+                case RacesActions.HANDLE_HEALER : {
+                    draft.isHealer = !state.isHealer;
+                    break;
+                }
+
+                default:
+                    return state;
+            }
+        })
+    );
