a4c9e6544ad8f8e783a060bfb2be3bc068425a17
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-05-04 14:08:56 +0200

    Refactorem Main container to BasicMode

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './MainContainer.scss';
+import './BasicMode/BasicMode.scss';
 import Button from './Button/Button';
 import ClassIcon from './ClassIcon/ClassIcon';
 import { connect } from 'react-redux';
@@ -9,6 +9,7 @@ import Human_icon from '../../assets/races_icons/Human_icon.png';
 import SwitchMode from './SwitchMode/SwitchMode';
 import {getClassesSelector} from "../../store/selectors/classes.selector";
 import {getAdvancedModeSelector, getFilteredRaces} from "../../store/selectors/races.selector";
+import BasicMode from "./BasicMode/BasicMode";
 
 export class MainContainer extends Component {
 	constructor(props) {
@@ -46,20 +47,8 @@ export class MainContainer extends Component {
 
 	render() {
 		return (
-			<div className="main-container">
-				<h2 className="text">WoW random class picker</h2>
-				<SwitchMode />
-				<div className={`${this.props.advancedMode ? 'test' : 'hide-items'}`}>
-				<div className="faction-role-container">
-					<FactionContainer />
-					<RolesContainer />
-				</div>
-				{this.props.advancedMode === true ? <img className="race-icon" src={Human_icon} /> : null}
-				</div>
-				<ClassIcon image={this.state.currentClassIcon} />
-				<h2 className="text">{this.state.currentClassName}</h2>
-				<Button roll={() => this.rollIcons()} />
-				{console.log('Filtered races:', this.props.filteredRaces)}
+			<div>
+	<BasicMode />
 			</div>
 		);
 	}
