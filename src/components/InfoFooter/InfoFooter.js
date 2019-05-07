d7856d128ead1eeda55b09e4fff76d223dcec442
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-05-07 16:53:20 +0200

    Add privacy policy link to the footer

--- src/components/InfoFooter/InfoFooter.js
+++ src/components/InfoFooter/InfoFooter.js
@@ -5,6 +5,7 @@ function InfoFooter(props) {
     return (
         <div className="info-footer">
             <p id="current-version" className="text-roller">Version: 0.6 beta</p>
+            <a id="privcy-policy" className="text-roller" href="#">Privacy policy and informations</a>
         </div>
     );
 }
