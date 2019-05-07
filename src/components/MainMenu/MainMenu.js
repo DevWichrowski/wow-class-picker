99a74fd08f63f4aff74c28755c1179115a5e10cc
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-05-07 10:11:46 +0200

    Change nav-link styles.

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -8,11 +8,11 @@ function MainMenu(props) {
     return (
         <div className="main-menu">
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="#home">WOW-RNG</Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Nav.Link className="text-roller" href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link className="text-roller" href="#home">Class generator</Nav.Link>
+                    <Nav.Link href="#features">Advanced generator</Nav.Link>
+                    <Nav.Link className="disabled-navlink" href="#pricing" disabled>Name generator</Nav.Link>
                 </Nav>
             </Navbar>
         </div>
