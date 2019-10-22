46b59b069d89d061b9be22488bab9fcdabae5cb2
DevWichrowski <wichrowski.patryk92@gmail.com>
2019-10-22 22:13:16 +0200

    Fixed problems with nav link

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import '../MainMenu/MainMenu.scss';
 import '../AdvancedMode/AdvancedMode.scss';
+import "../../styles/animations.scss";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import wowRngLogo from "../../assets/wowrng-logo.png"
+import {NavLink} from "react-router-dom";
 
-function MainMenu() {
+function MainMenu(props) {
 
     return (
-        <div className="main-menu">
+        <div className="main-menu slide-in-top">
             <AppBar position="static" className="main-menu-bar">
+                {console.log('props.location.pathname', props)}
                 <Toolbar>
-                    <img src={wowRngLogo} className="wow-rng-logo" />
-                    <Button className="nav-link">Class generator</Button>
-                    <Button className="nav-link">Advanced generator</Button>
-                    <Button className="nav-link">Classic generator</Button>
-                    <Button className="nav-link">Name generator</Button>
+                    <img src={wowRngLogo} className="wow-rng-logo"/>
+                    <NavLink exact to="/" activeClassName="activated-nav">
+                        <Button className="nav-link">Class generator</Button>
+                    </NavLink>
+                    <NavLink to="advanced-mode" activeClassName="activated-nav">
+                        <Button className="nav-link">Advanced generator</Button>
+                    </NavLink>
+                    <Button className="nav-link-disabled" disabled>Classic generator</Button>
+                    <Button className="nav-link-disabled" disabled>Name generator</Button>
+
+                    <NavLink to="/about" activeClassName="activated-nav">
+                        <Button className="nav-link">About</Button>
+                    </NavLink>
                 </Toolbar>
             </AppBar>
         </div>
