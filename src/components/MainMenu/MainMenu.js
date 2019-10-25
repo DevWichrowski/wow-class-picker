c147254b4f479c05586bd97e0a4f0aabdcab381d
DevWichrowski <wichrowski.patryk92@gmail.com>
2019-10-25 20:45:54 +0200

    minor changes

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../MainMenu/MainMenu.scss';
 import '../AdvancedMode/AdvancedMode.scss';
 import "../../styles/animations.scss";
@@ -6,81 +6,84 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import wowRngLogo from "../../assets/wowrng-logo.png"
-import { NavLink } from "react-router-dom";
-import Dropdown from "antd/es/dropdown";
-import Menu from "antd/es/menu";
+import {NavLink} from "react-router-dom";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Icon from "@material-ui/core/Icon";
 
 class MainMenu extends Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {
-			width: 0,
-			height: 0
-		};
-	}
+        this.state = {
+            width: 0,
+            height: 0,
+            anchorEl: null,
+        };
+    }
 
-	updateDimensions = () => {
-		this.setState({width: window.innerWidth, height: window.innerHeight});
-	};
+    updateDimensions = () => {
+        this.setState({width: window.innerWidth, height: window.innerHeight});
+    };
 
-	componentDidMount() {
-		window.addEventListener('resize', this.updateDimensions);
-	}
+    componentDidMount() {
+        window.addEventListener('resize', this.updateDimensions);
+    }
 
-	render() {
+    handleClick = event => {
+        this.setState({anchorEl: event.currentTarget});
+    };
 
-		const menu = (
-			<Menu>
-				<Menu.Item>
-					<a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
-						1st menu item
-					</a>
-				</Menu.Item>
-				<Menu.Item>
-					<a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
-						2nd menu item
-					</a>
-				</Menu.Item>
-				<Menu.Item>
-					<a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
-						3rd menu item
-					</a>
-				</Menu.Item>
-			</Menu>
-		);
+    handleClose = () => {
+        this.setState({anchorEl: null});
+    };
 
-		return (
-			<div className="main-menu slide-in-top">
-				<AppBar position="static" className="main-menu-bar">
-					<Toolbar>
-						<img src={wowRngLogo} className="wow-rng-logo"/>
-						{this.state.width >= 900 ? <div className="flex-container">
-							<div>
-								<NavLink exact to="/" activeClassName="activated-nav">
-									<Button className="nav-link">Class generator</Button>
-								</NavLink>
-								<NavLink to="advanced-mode" activeClassName="activated-nav">
-									<Button className="nav-link">Advanced generator</Button>
-								</NavLink>
-								<Button className="nav-link-disabled" disabled>Classic generator</Button>
-								<Button className="nav-link-disabled" disabled>Name generator</Button>
-							</div>
-							<div>
-								<NavLink to="/about" activeClassName="activated-nav">
-									<Button className="nav-link">About</Button>
-								</NavLink>
-							</div>
-						</div> :
-							<Dropdown overlay={menu} placement="bottomRight">
-								<Button>bottomRight</Button>
-							</Dropdown>
-						}
-					</Toolbar>
-				</AppBar>
-			</div>
-		)
-	}
+    render() {
+
+        return (
+            <div className="main-menu slide-in-top">
+                <AppBar position="static" className="main-menu-bar">
+                    <Toolbar>
+                        <img src={wowRngLogo} className="wow-rng-logo"/>
+                        {this.state.width >= 900 ? <div className="flex-container">
+                                <div>
+                                    <NavLink exact to="/" activeClassName="activated-nav">
+                                        <Button className="nav-link">Class generator</Button>
+                                    </NavLink>
+                                    <NavLink to="advanced-mode" activeClassName="activated-nav">
+                                        <Button className="nav-link">Advanced generator</Button>
+                                    </NavLink>
+                                    <Button className="nav-link-disabled" disabled>Classic generator</Button>
+                                    <Button className="nav-link-disabled" disabled>Name generator</Button>
+                                </div>
+                                <div>
+                                    <NavLink to="/about" activeClassName="activated-nav">
+                                        <Button className="nav-link">About</Button>
+                                    </NavLink>
+                                </div>
+                            </div> :
+                            <div className="mobile-menu">
+                                <Button aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick}>
+                                    <Icon className="mobile-menu-icon">menu</Icon>
+                                </Button>
+                                <Menu
+                                    id="simple-menu"
+                                    anchorEl={this.state.anchorEl}
+                                    keepMounted
+                                    open={Boolean(this.state.anchorEl)}
+                                    onClose={this.handleClose}
+                                >
+                                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                                    <MenuItem>Logout</MenuItem>
+                                </Menu>
+                            </div>
+                        }
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
 }
 
 export default MainMenu;
